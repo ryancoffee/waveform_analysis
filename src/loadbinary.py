3e7bab621d6a89080b1cc0cb0f8d63d658e602bf
Ryan Coffee <coffeer76@gmail.com>
2020-03-11 00:41:34 -0700

    incremental

--- src/loadbinary.py
+++ src/loadbinary.py
@@ -13,16 +13,17 @@ def getHeaderBytes(fname):
     nseek = int(len(buf) - nvals)
     return (nseek,nvals)
 
-def laodArrayBytes(fname,nseek):
+def loadArrayBytes(fname,nseek):
     f = open(fname,'br')
+    f.seek(nseek)
     buf = f.read()
     f.close()
-    return np.array([np.int8(v) for v in buf])
+    return np.array([np.int8(v) for v in buf],dtype=np.int8)
 
 
 def main():
     if len(sys.argv[1])==1:
-        print('syntax is: loadbinary.py <path> <C1> <nwaves>')
+        print('syntax is: ./src/loadbinary.py <path> <C1 or so> <nwaves>')
     path = 'data_fs'
     if (len(sys.argv)>1):
         path = sys.argv[1]
@@ -36,11 +37,20 @@ def main():
     wv = int(0)
     fname = '%s/%s--%s--%05i.trc'%(path,ch,ch_trig,wv)
     (nseek,nvals) = getHeaderBytes(fname)
+    #parseddata = np.zeros(nvals,dtype=np.float32)
     data = np.zeros(nvals,dtype=np.int8)
+    thresh = 6
+    if (len(sys.argv)>4):
+        thresh = int(sys.argv[4])
+
     for wv in range(nwaves):
         fname = '%s/%s--%s--%05i.trc'%(path,ch,ch_trig,wv)
+        #parseddata = lecroyparser.ScopeData(fname)
         oname = '%s/%s--%s--%05i.out'%(path,ch,ch_trig,wv)
-        np.savetxt(oname,laodArrayBytes(fname,nseek),fmt = '%i')
+        data = loadArrayBytes(fname,nseek)
+        if (wv%10==0) or (np.max(data)>np.mean(data)+thresh):
+            np.savetxt(oname,data,fmt = '%i')
+        #np.savetxt(oname + 'parser',parseddata.y,fmt = '%.4f')
     return
 
 if __name__ == "__main__":
