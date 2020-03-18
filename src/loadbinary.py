0f9f5290a1d289d4a5bc5d93690887e751b2c31f
Ryan Coffee <coffeer76@gmail.com>
2020-03-17 23:59:28 -0700

    histogramming CookieBox

--- src/loadbinary.py
+++ src/loadbinary.py
@@ -6,6 +6,28 @@ import h5py
 import sys
 import os
 
+def sigmoid(x,c,w):
+    return 1./(1.+np.exp(-(x-c)/w))
+
+def zeroCrossings(data,thresh):
+    tofs = []
+    i = int(0)
+    sz = data.shape[0]
+    while i < data.shape[0]-1:
+        while data[i] < thresh:
+            i += 1
+            if i == sz: break
+        if i == sz: break
+        while data[i] > 0:
+            i += 1
+            if i == sz: break
+        tofs = tofs + [1./float(data[i]-data[i-1])*data[i] + float(i)]
+        if i == sz: break
+        while data[i] < 0:
+            i += 1
+            if i == sz: break
+    return tofs 
+
 def getHeaderBytes(fname):
     data = lecroyparser.ScopeData(fname)
     nvals = len(data.y)
@@ -45,6 +67,8 @@ def getacFilter(data,FREQ,cut = 0.2):
     AC[inds] = 0.5*(1. - np.cos(FREQ[inds]*np.pi/cut))
     return AC
 
+
+
 def main():
     if len(sys.argv[1])==1:
         print('syntax is: ./src/loadbinary.py <path> <fname_front, not extension> <nwaves>')
@@ -59,17 +83,23 @@ def main():
         nwaves = int(sys.argv[3])
     wv = int(0)
     fname = '%s/%s%05i.trc'%(path,fname_front,wv)
+    hname = '%s/%s.hist'%(path,fname_front)
     (nseek,nvals) = getHeaderBytes(fname)
     
     parseddata = np.zeros(nvals,dtype=np.float32)
     data = np.zeros(nvals,dtype=np.int16)
     FREQ = np.fft.fftfreq(data.shape[0],1./40.) # 1/sampling rate in GHz
     W = getWeinerFilter(data,FREQ,cut = 3.0,noise = 0.1)
+    W_lowpass = getWeinerFilter(data,FREQ,cut = 1.0,noise = 0.0001)
     AC = getacFilter(data,FREQ,cut = 0.05)
     thresh = 6
+    negation = 1 # set to 0 for positive going signals, 1 or -1 for negative going
     if (len(sys.argv)>4):
         thresh = int(sys.argv[4])
 
+    tofs = []
+    hout = np.zeros(2**12,int)
+    bins = np.linspace(0,2**14,hout.shape[0]+1)
     for wv in range(nwaves):
         fname = '%s/%s%05i.trc'%(path,fname_front,wv)
         if not os.path.exists(fname):
@@ -77,9 +107,20 @@ def main():
         #parseddata = lecroyparser.ScopeData(fname)
         oname = '%s/%s%05i.out'%(path,fname_front,wv)
         data = loadArrayBytes_int16(fname,nseek,nvals,byteorder = 'little')
-        DATA = np.fft.fft(data)
-        out = np.fft.ifft(DATA*W*AC)
-        np.savetxt(oname,np.column_stack((FREQ,np.abs(DATA),np.abs(DATA*W*AC),data,out.real)),fmt = '%f')
+        y = np.fft.ifft(np.fft.fft(data)*W*AC).real
+        dy = np.fft.ifft(1j*FREQ*np.fft.fft(data)*W*AC).real
+        #DATA = np.fft.fft(sigmoid(y,2500,1000))*W_lowpass
+        #IDATA = np.fft.fft(sigmoid(y,2500,1000))*np.arange(y.shape[0])*W_lowpass
+        #num = np.fft.ifft(IDATA).real
+        #denom = np.fft.ifft(DATA).real
+        tofs = tofs + zeroCrossings((y * dy)/float(y.shape[0]),1000)
+        if wv%1000 == 0:
+            np.savetxt(oname,np.column_stack((data,y,dy)),fmt= '%f')
+            hout += np.histogram(tofs,bins)[0]
+            np.savetxt(hname,hout,fmt='%i')
+            tofs = []
+    hout += np.histogram(tofs,bins)[0]
+    np.savetxt(hname,hout,fmt='%i')
     return
 
 if __name__ == "__main__":
